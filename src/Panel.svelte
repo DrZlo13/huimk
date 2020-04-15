8e65ad4d50269b49c79c2aea07de3f6710d29875
Dr_Zlo <who.just.the.doctor@gmail.com>
2020-04-15 16:50:10 +1000

    Переработка панели, уход от общего тега стилизации

--- src/Panel.svelte
+++ src/Panel.svelte
@@ -1,6 +1,17 @@
 <script>
-    export let style;
+    export let style = null;
+    export let justify = false;
+	export let offsetTop = 0;
+	export let offsetBottom = 0;
+	export let offsetLeft = 0;
+	export let offsetRight = 0;
 </script>
-<div style={style}>
-	<slot></slot>
+<div style="{`
+        	padding-top: ${offsetTop}px;
+        	padding-bottom: ${offsetBottom}px;
+        	padding-left: ${offsetLeft}px;
+        	padding-right: ${offsetRight}px;
+        	${justify === 'around' ? 'display: flex; justify-content: space-around' : ''};
+    	`}">
+	<slot/>
 </div>
