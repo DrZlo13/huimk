d82079ce4afe3086047e16e3dba3524562698f24
Dr_Zlo <who.just.the.doctor@gmail.com>
2020-04-15 18:52:36 +1000

    Добавлен выбор направления элементов на панели. Добавлено выравнивание по центру.

--- src/Panel.svelte
+++ src/Panel.svelte
@@ -5,13 +5,17 @@
 	export let offsetBottom = 0;
 	export let offsetLeft = 0;
 	export let offsetRight = 0;
+	export let direction = 'row';
 </script>
 <div style="{`
+            display: flex;
         	padding-top: ${offsetTop}px;
         	padding-bottom: ${offsetBottom}px;
         	padding-left: ${offsetLeft}px;
         	padding-right: ${offsetRight}px;
-        	${justify === 'around' ? 'display: flex; justify-content: space-around' : ''};
+        	${justify === 'around' ? 'justify-content: space-around' : ''};
+        	${justify === 'center' ? 'justify-content: center' : ''};
+        	${direction === 'column' ? 'flex-direction: column' : ''};
     	`}">
 	<slot/>
 </div>
